7180c83feec7849942fc4a557b4a6e8a5142ce76
nastyada <nastyadanilkina1294@yandex.ru>
2017-11-23 10:27:08 +0300

    Repaire problem with absolute path

--- src/test/java/AuthService/CreateUser.java
+++ src/test/java/AuthService/CreateUser.java
@@ -1,10 +1,8 @@
 package AuthService;
 
 import HelpClass.BaseClass;
-import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
-import org.openqa.selenium.support.PageFactory;
 import org.testng.annotations.Test;
 import ru.yandex.qatools.allure.annotations.Step;
 import ru.yandex.qatools.allure.annotations.Title;
@@ -44,13 +42,12 @@ public class CreateUser extends BaseClass {
     public void createUser( ) {
         login( );
         String loginName = "testUser" + LocalDateTime.now( ).toString( ).replace( ":", "_" );
-        createUser( loginName );
-        //assertEquals( wd.findElement( By.xpath( "//table/tbody/tr[last()]/td[2]" ) ).getText( ), loginName );
+        createNewUser( loginName );
         assertEquals( nameLoginObject.getText(), loginName );
     }
 
     @Step("Создание новго пользователя")
-    private void createUser( String login ) {
+    private void createNewUser( String login ) {
         setLogin( login );
         setPassword( "test" );
         setSurname( "Testov" );
@@ -65,15 +62,11 @@ public class CreateUser extends BaseClass {
 
     @Step("9. Сохранине")
     private void clickToSave( ) {
-       // wd.findElement( By.xpath( "//div[2]/div/div[2]/div/div[1]/div[3]/div/button[2]" ) ).click( );
         saveBTN.click();
     }
 
     @Step("8. Ввод роли {0}")
     private void setRole( String role ) {
-        /*wd.findElement( By.id("role")).click();
-        wd.findElement(By.id("role")).clear();
-        wd.findElement(By.id("role")).sendKeys( role );*/
         roleTB.click( );
         roleTB.clear( );
         roleTB.sendKeys( role );
@@ -81,9 +74,6 @@ public class CreateUser extends BaseClass {
 
     @Step("7. Ввод номера телефона {0}")
     private void setPhoneNumber( String phoneNumber ) {
-        /*wd.findElement( By.xpath("//div[@class='ant-modal-body']/form/div[4]/div[2]/div[2]/div/div/div/div/ul/li/div/input")).click();
-        wd.findElement(By.xpath("//div[@class='ant-modal-body']/form/div[4]/div[2]/div[2]/div/div/div/div/ul/li/div/input")).clear();
-        wd.findElement(By.xpath("//div[@class='ant-modal-body']/form/div[4]/div[2]/div[2]/div/div/div/div/ul/li/div/input")).sendKeys( phoneNumber );*/
         phoneNumberTB.click( );
         phoneNumberTB.clear( );
         phoneNumberTB.sendKeys( phoneNumber );
@@ -91,9 +81,6 @@ public class CreateUser extends BaseClass {
 
     @Step("6. Ввод email {0}")
     private void setEmail( String email ) {
-       /* wd.findElement( By.xpath("//div[@class='ant-modal-body']/form/div[4]/div[1]/div[2]/div/div[1]/div/div/ul/li/div/input")).click();
-        wd.findElement(By.xpath("//div[@class='ant-modal-body']/form/div[4]/div[1]/div[2]/div/div[1]/div/div/ul/li/div/input")).clear();
-        wd.findElement(By.xpath("//div[@class='ant-modal-body']/form/div[4]/div[1]/div[2]/div/div[1]/div/div/ul/li/div/input")).sendKeys( email );*/
         emailTB.click( );
         emailTB.clear( );
         emailTB.sendKeys( email );
@@ -101,9 +88,6 @@ public class CreateUser extends BaseClass {
 
     @Step("5. Ввод отчества {0}")
     private void setMiddleName( String middleName ) {
-        /*wd.findElement( By.id("middleName")).click();
-        wd.findElement(By.id("middleName")).clear();
-        wd.findElement(By.id("middleName")).sendKeys( middleName );*/
         middleNameTB.click( );
         middleNameTB.clear( );
         middleNameTB.sendKeys( middleName );
@@ -111,9 +95,6 @@ public class CreateUser extends BaseClass {
 
     @Step("4. Ввод имени {0}")
     private void setFirstName( String firstName ) {
-       /* wd.findElement( By.id("firstName")).click();
-        wd.findElement(By.id("firstName")).clear();
-        wd.findElement(By.id("firstName")).sendKeys( firstName );*/
         firstNameTB.click( );
         firstNameTB.clear( );
         firstNameTB.sendKeys( firstName );
@@ -121,9 +102,6 @@ public class CreateUser extends BaseClass {
 
     @Step("3. Ввод фамилии {0}")
     private void setSurname( String surname ) {
-        /*wd.findElement( By.id("lastName")).click();
-        wd.findElement(By.id("lastName")).clear();
-        wd.findElement(By.id("lastName")).sendKeys(surname);*/
         lastNameTB.click( );
         lastNameTB.clear( );
         lastNameTB.sendKeys( surname );
@@ -131,12 +109,6 @@ public class CreateUser extends BaseClass {
 
     @Step("2. Ввод пароля {0}")
     private void setPassword( String password ) {
-        /*wd.findElement( By.id("password")).click();
-        wd.findElement(By.id("password")).clear();
-        wd.findElement(By.id("password")).sendKeys( password );
-        wd.findElement(By.id("passwordRepeat")).click();
-        wd.findElement(By.id("passwordRepeat")).clear();
-        wd.findElement(By.id("passwordRepeat")).sendKeys(password);*/
         passwordTB.click( );
         passwordTB.clear( );
         passwordTB.sendKeys( password );
@@ -148,10 +120,6 @@ public class CreateUser extends BaseClass {
 
     @Step("1. Ввод логина {0}")
     private void setLogin( String login ) {
-        /*wd.findElement( By.xpath("//*[@id=\"authorization\"]/div/div[2]/div[1]/div/span[3]/button")).click();
-        wd.findElement(By.cssSelector("input.ant-input.ant-select-search__field")).click();
-        wd.findElement(By.cssSelector("input.ant-input.ant-select-search__field")).clear();
-        wd.findElement(By.cssSelector("input.ant-input.ant-select-search__field")).sendKeys( login );*/
         buttonAddBTN.click( );
         loginNameTB.clear( );
         loginNameTB.clear( );
