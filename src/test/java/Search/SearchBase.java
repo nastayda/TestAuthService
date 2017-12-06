d7a00a09da511d5c5a8fe10073041a741bb1fe97
nastyada <nastyadanilkina1294@yandex.ru>
2017-12-06 18:43:42 +0300

    Add test plan for seraching

--- src/test/java/Search/SearchBase.java
+++ src/test/java/Search/SearchBase.java
@@ -1,11 +1,14 @@
 package Search;
 
 import HelpClass.BaseClass;
+import com.sun.xml.bind.v2.runtime.unmarshaller.XsiNilLoader;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.testng.annotations.Test;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 @Test
@@ -17,19 +20,31 @@ public class SearchBase extends BaseClass {
     @FindBy(xpath = "//span[@class='ant-input-group-wrapper']/span/input")
     WebElement serachAreaTB;
     @FindBy(xpath = "//*[@id=\"authorization\"]/div/div[2]/div[2]/div/div/div/div/div/div/div[1]/table/thead/tr/th")
-    List<WebElement> tableHeads;
+    List<WebElement> tableHeader;
+    @FindBy(xpath = "//*[@id=\"authorization\"]/div/div[2]/div[2]/div/div/div/div/div/div/div[2]/table/tbody/tr")
+    List<WebElement> tableRow;
 
+    //Туповатая идея(других пока нет): определить какие есть элементы в таблице(по заголовкам),
+    // если такие же элементы есть в списке для поиска, то получаем текст первый не пустой
+    // и вставляем его по выбранному критерию поиска в текстовый блок
+    // ИДЕЯ 2: открыть форму посмотреть какие есть поля и искать эти поля в списке... - наверное будет круче так сделать!
     public void search( ) {
         login( );
-        menu.click( );
-        waitSomeMillisec(  500 );
-        for (int i =0; i < menuPoint.size(); i++){
-            System.out.println("Text i="+i+" " + menuPoint.get( i ).getText() );
+        //Разбить полученную строку на массив и из полученного массива получить нужный элемент
+        System.out.println( Arrays.asList( tableRow.get( 0 ).getText( ).split( " " ) ).get( 0 ) );
+        /*menu.click( );
+        waitSomeMillisec( 500 );
+        for (int i = 0; i < menuPoint.size( ); i++) {
+            for (int j =0; j< tableHeader.size(); j++){
+                if (menuPoint.get( i ).getText().equals( tableHeader.get( j ).getText() )){
+                    tableRow.get( j ).getText();
+                }
+            }
+            System.out.println( "Text i=" + i + " " + menuPoint.get( i ).getText( ) );
         }
         menuPoint.get( 6 ).click( );
         serachAreaTB.click( );
         serachAreaTB.clear( );
-        serachAreaTB.sendKeys( "test" );
+        serachAreaTB.sendKeys( "test" );*/
     }
-
 }
