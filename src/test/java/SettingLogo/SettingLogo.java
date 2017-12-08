e002f04d41ad54c828ae85fcb110ee728317b6d8
nastyada <nastyadanilkina1294@yandex.ru>
2017-12-08 09:59:35 +0300

    Add steps

--- src/test/java/SettingLogo/SettingLogo.java
+++ src/test/java/SettingLogo/SettingLogo.java
@@ -4,19 +4,17 @@ import HelpClass.BaseClass;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.testng.annotations.Test;
+import ru.yandex.qatools.allure.annotations.Step;
 
 import static org.testng.Assert.assertEquals;
 
 public class SettingLogo extends BaseClass {
     @FindBy(xpath = "//*[@id=\"authorization\"]/div/div[2]/div[1]/div/span[1]/button")
     WebElement settingBTN;
-
     @FindBy(xpath = "//form/div[2]/div/div[2]/div/input")
     WebElement urlTB;
-
     @FindBy(xpath = "//div[2]/div/div[2]/div/div[1]/div[3]/div/button[2]")
     WebElement saveBTN;
-
     @FindBy(xpath = "//*[@id=\"authorization\"]/div/div[1]/div")
     WebElement goToLogo;
 
@@ -29,20 +27,22 @@ public class SettingLogo extends BaseClass {
         saveNewUrl( newUrl );
         goToLogoPage( );
         assertEquals(newUrl, wd.getCurrentUrl().toString());
+        //Change to the first state
         wd.navigate().back();
+        goToChangeLogoUrlForm( );
         saveNewUrl(textBefor);
     }
-
+    @Step("")
     public void goToLogoPage( ) {
         goToLogo.click();
     }
-
+    @Step("")
     public void saveNewUrl( String url ) {
         urlTB.clear();
         urlTB.sendKeys( url );
         saveBTN.click();
     }
-
+    @Step("")
     public void goToChangeLogoUrlForm( ) {
         settingBTN.click();
         urlTB.click();
