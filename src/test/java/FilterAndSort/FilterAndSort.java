1219bfe3cf2202b26c37a00497f2e5bca9501c15
nastyada <nastyadanilkina1294@yandex.ru>
2018-01-11 12:56:41 +0300

    Problem with count of li in list - echa befor count sums with next count

--- src/test/java/FilterAndSort/FilterAndSort.java
+++ src/test/java/FilterAndSort/FilterAndSort.java
@@ -16,7 +16,7 @@ public class FilterAndSort extends BaseClass {
     //*[@id="root"]/div/div[2]/div[2]/div/div[1]/div/div/div/div/div[1]/table/thead/tr/th[4]/span/i
     //*[@id="root"]/div/div[2]/div[2]/div/div[1]/div/div/div/div/div[1]/table/thead/tr/th[8]/span/i
     @FindBy(xpath = "//thead[@class='ant-table-thead']/tr/th/span/i")
-    List <WebElement> filter;
+    List<WebElement> filter;
     //div[@class='ant-table-filter-dropdown']/ul/li[2]/label/span/input
     //*[@id="root"]/div/div[2]/div[2]/div/div[2]/div/div/div/ul/li[1]
     //*[@id="root"]/div/div[2]/div[2]/div/div[3]/div/div/div/ul/li[1]
@@ -35,7 +35,7 @@ public class FilterAndSort extends BaseClass {
     public void applyFilterTest( ) {
         login( );
         // Развернуть окно до размеров 1920 х 1080, чтобы добраться до последнего элемента
-        wd.manage().window().setSize( new Dimension( 1920, 1080 ) );
+        wd.manage( ).window( ).setSize( new Dimension( 1920, 1080 ) );
         //System.out.println(filter.size() );
         applyFilter( );
     }
@@ -43,18 +43,20 @@ public class FilterAndSort extends BaseClass {
     public void applyFilter( ) {
         //Внешний цикл по колонкам
         for (int i = 0; i < filter.size( ); i++) {
-            filter.get( i ).click();
+            filter.get( i ).click( );
             //Внутренний цикл по значениям фильтра
+            System.out.println( meaningFilterCHBX.size( ) );
             for (int j = 0; j < meaningFilterCHBX.size( ); j++) {
-                if (!isEmpty(meaningFilterCHBX.get( j ).getText())){
-                    System.out.println( meaningFilterCHBX.get( j ).getText() );
-                    int countBefore = countByCriterion(meaningFilterCHBX.get( j ).getText(), j);
-                    meaningFilterCHBX.get( j ).click();
+                if (!isEmpty( meaningFilterCHBX.get( j ).getText( ) )) {
+                    System.out.println( meaningFilterCHBX.get( j ).getText( ) );
+                    int countBefore = countByCriterion( meaningFilterCHBX.get( j ).getText( ), j );
+                    meaningFilterCHBX.get( j ).click( );
                     waitSomeMillisec( 1000 );
-                    okBTN.click();
-                    int countAfter = tableRow.size();
+                    okBTN.click( );
+                    int countAfter = tableRow.size( );
                     softAssert.assertEquals( countBefore, countAfter );
-                    j=meaningFilterCHBX.size( );
+                    //j = meaningFilterCHBX.size( );
+                    break;
                 }
             }
         }
@@ -62,7 +64,7 @@ public class FilterAndSort extends BaseClass {
 
     private int countByCriterion( String searchCriterion, int startJ ) {
         int k = 0;
-        for (int j = startJ; j < tableCol.size( ); j = j + filter.size( )+1) {
+        for (int j = startJ; j < tableCol.size( ); j = j + filter.size( ) + 1) {
             if (tableCol.get( j ).getText( ).contains( searchCriterion )) {
                 k++;
             }
