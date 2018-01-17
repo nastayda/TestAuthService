0827d2a496352ced8f9fa8fe6bc86239c4990d5a
nastyada <nastyadanilkina1294@yandex.ru>
2018-01-17 11:58:50 +0300

    Add sort class and methods

--- src/test/java/FilterAndSort/Sort.java
+++ src/test/java/FilterAndSort/Sort.java
@@ -1,11 +1,18 @@
 package FilterAndSort;
 
 import HelpClass.BaseClass;
+import com.google.common.collect.Lists;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.testng.annotations.Test;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
+
+import static org.jvnet.jaxb2_commons.lang.StringUtils.isEmpty;
+import static org.testng.AssertJUnit.assertEquals;
 
 @Test
 public class Sort extends BaseClass {
@@ -26,20 +33,50 @@ public class Sort extends BaseClass {
         login( );
         resizeWindow( 1920, 1080 );
         sortByAlphabet( );
+        softAssert.assertAll();
+
+    }
+
+    public static void sortList( ArrayList<String> aItems ) {
+        Collections.sort( aItems, String.CASE_INSENSITIVE_ORDER );
     }
 
     public void sortByAlphabet( ) {
-       /* for (int i = 1; i < downArrow.size( ); i++) {
+        for (int i = 0; i < downArrow.size( ); i++) {
+            //По убыванию
             downArrow.get( i ).click( );
             waitSomeMillisec( 1000 );
-            upArrow.get( i ).click( );
-            waitSomeMillisec( 1000 );
-        }*/
-        for (int i = 0; i < tableCol.size( )/tableRow.size(); i++) {
-            for (int j = i; j < tableCol.size( ); j=j+tableCol.size( )/tableRow.size()) {
-                System.out.println( tableCol.get( j ).getText( ) );
+            //с какой колонки начинаем
+            for (int k = i + 1; k < i + 2; k++) {
+                List<String> sortedByInterface = new ArrayList<>( );
+                //какой конкретно эелемент брать
+                for (int j = k; j < tableCol.size( ); j = j + tableCol.size( ) / tableRow.size( )) {
+                    if (!isEmpty( tableCol.get( j ).getText( ) )) {
+                        sortedByInterface.add( tableCol.get( j ).getText( ) );
+                    }
+                }
+                ArrayList<String> sortedByRule = new ArrayList<>( sortedByInterface );
+                //сортировка по алфавиту
+                sortList( sortedByRule );
+                //реверсия т.к. тут нажат спан по убыванию
+                List<String> reverseOrder = Lists.reverse( sortedByRule );
+                softAssert.assertEquals( reverseOrder, sortedByInterface, "Проверка по убыванию провалилась." );
             }
-            System.out.println("---");
+            //По возрастанию
+            upArrow.get( i ).click( );
+            for (int k = i + 1; k < i + 2; k++) {
+                List<String> sortedByInterface = new ArrayList<>( );
+                //какой конкретно эелемент брать
+                for (int j = k; j < tableCol.size( ); j = j + tableCol.size( ) / tableRow.size( )) {
+                    if (!isEmpty( tableCol.get( j ).getText( ) )) {
+                        sortedByInterface.add( tableCol.get( j ).getText( ) );
+                    }
+                }
+                ArrayList<String> sortedByRule = new ArrayList<>( sortedByInterface );
+                //сортировка по алфавиту
+                sortList( sortedByRule );
+                softAssert.assertEquals( sortedByRule, sortedByInterface, "Проверка по возрастанию провалилась." );
+            }
         }
     }
 }
