197517e3031c262d7d5845d84c1ee675272f42b4
nastyada <nastyadanilkina1294@yandex.ru>
2018-01-23 11:46:26 +0300

    Add setLogo method

--- src/test/java/SettingLogo/SettingLogo.java
+++ src/test/java/SettingLogo/SettingLogo.java
@@ -1,52 +1,78 @@
 package SettingLogo;
 
 import HelpClass.BaseClass;
+//import com.sun.jna.platform.FileUtils;
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 import org.testng.annotations.Test;
 import ru.yandex.qatools.allure.annotations.Step;
 
+import java.io.File;
+import java.io.IOException;
+
 import static org.testng.Assert.assertEquals;
 
 public class SettingLogo extends BaseClass {
-    @FindBy(xpath = "//*[@id=\"authorization\"]/div/div[2]/div[1]/div/span[1]/button")
+    @FindBy(xpath = "//*[@id=\"root\"]/div/div[1]/div/div[2]/div/div/div[1]/div/button")
     WebElement settingBTN;
+    @FindBy(xpath = "//div[2]/div/div/ul/li[1]")
+    WebElement getSettingLogoSubmenuBTN;
     @FindBy(xpath = "//form/div[2]/div/div[2]/div/input")
     WebElement urlTB;
-    @FindBy(xpath = "//div[2]/div/div[2]/div/div[1]/div[3]/div/button[2]")
+    @FindBy(xpath = "//div[3]/div/div[2]/div/div[1]/div[3]/div/button[2]")
     WebElement saveBTN;
-    @FindBy(xpath = "//*[@id=\"authorization\"]/div/div[1]/div")
+    @FindBy(xpath = "//div[@id='root']/div/div[1]/div/div[1]/div/div[1]/img")
     WebElement goToLogo;
+    @FindBy(xpath = "//*[@id=\"root\"]")
+    WebElement body;
 
     @Test
-    public void setLogoUrl(){
-       // login();
-        PageFactory.initElements(wd, this);
+    public void setLogoUrl( ) throws IOException {
+        // login();
+        PageFactory.initElements( wd, this );
         goToChangeLogoUrlForm( );
         String textBefor = urlTB.getAttribute( "value" );
-        String newUrl ="https://ya.ru/";
+        String newUrl = "https://ya.ru/";
         saveNewUrl( newUrl );
+        waitSomeMillisec( 2000 );
         goToLogoPage( );
-        assertEquals(newUrl, wd.getCurrentUrl().toString());
+        assertEquals( wd.getCurrentUrl( ).toString( ), newUrl, "URL не был изменен. " );
         //Change to the first state
-        wd.navigate().back();
+        wd.navigate( ).back( );
         goToChangeLogoUrlForm( );
-        saveNewUrl(textBefor);
+        saveNewUrl( textBefor );
     }
+
     @Step("Переход на страницу с логотипом")
     public void goToLogoPage( ) {
-        goToLogo.click();
+        goToLogo.click( );
     }
+
     @Step("Сохранить новый URL {0}")
-    public void saveNewUrl( String url ) {
-        urlTB.clear();
+    public void saveNewUrl( String url ) throws IOException {
+        urlTB.click( );
+        urlTB.clear( );
         urlTB.sendKeys( url );
-        saveBTN.click();
+        saveBTN.click( );
+        waitSomeMillisec( 2000 );
+        createSkreenshot( );
+        body.click();
     }
+
+    public void createSkreenshot( ) throws IOException {
+        File scrFile = ((TakesScreenshot)wd).getScreenshotAs( OutputType.FILE);
+// Now you can do whatever you need to do with it, for example copy somewhere
+        FileUtils.copyFile(scrFile, new File("c:\\tmp\\screenshot.png"));
+
+    }
+
     @Step("Прейти по изменненому URL")
     public void goToChangeLogoUrlForm( ) {
-        settingBTN.click();
-        urlTB.click();
+        settingBTN.click( );
+        getSettingLogoSubmenuBTN.click( );
     }
 }
